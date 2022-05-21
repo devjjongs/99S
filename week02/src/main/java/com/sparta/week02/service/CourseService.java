4820ad202ec130bd40d89e74b1e610dc52915053
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-22 02:04:45 +0900

    2-8강 Lombok, Dto
    Lombok의 annotation 사용 연습
    Dto의 이해 및 코드 연습

--- week02/src/main/java/com/sparta/week02/service/CourseService.java
+++ week02/src/main/java/com/sparta/week02/service/CourseService.java
@@ -2,10 +2,13 @@ package com.sparta.week02.service;
 
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.domain.CourseRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+//@RequiredArgsConstructor
 @Service // 스프링에게 이 클래스는 서비스임을 명시
 public class CourseService {
 
@@ -19,11 +22,11 @@ public class CourseService {
     }
 
     @Transactional // SQL 쿼리가 일어나야 함을 스프링에게 알려줌
-    public Long update(Long id, Course course) {
+    public Long update(Long id, CourseRequestDto requestDto) {
         Course course1 = courseRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
         );
-        course1.update(course);
+        course1.update(requestDto);
         return course1.getId();
     }
 }
