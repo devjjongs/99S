954b6feb16db0dcafbd16a9c97ceeb68bce9beb4
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-24 16:26:54 +0900

    3-5강 Controller만들기
    MemoController 클래스 생성 및 CRUD 기능 추가

--- week03/src/main/java/com/sparta/week03/service/MemoService.java
+++ week03/src/main/java/com/sparta/week03/service/MemoService.java
@@ -6,12 +6,15 @@ import com.sparta.week03.domain.MemoRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @RequiredArgsConstructor // final 선언한 것을 포함시키기 위해서 필요
 @Service
 public class MemoService {
 
     private final MemoRepository memoRepository;
 
+    @Transactional
     public Long update(Long id, MemoRequestDto requestDto) {
         Memo memo = memoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("아이디가 존재하지 않습니다."));
         memo.update(requestDto);
