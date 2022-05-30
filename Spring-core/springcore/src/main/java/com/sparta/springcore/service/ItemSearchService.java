7289261f7385d786e732f2f3b8fec25daf1d85b3
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-30 18:26:46 +0900

    1-19강 1주차 숙제
    ItemSearchController 클래스를 Controller 와 Service 2개 파일로 분리 작성하기

--- Spring-core/springcore/src/main/java/com/sparta/springcore/service/ItemSearchService.java
+++ Spring-core/springcore/src/main/java/com/sparta/springcore/service/ItemSearchService.java
@@ -1,41 +1,23 @@
-package com.sparta.springcore.controller;
+package com.sparta.springcore.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.springcore.dto.ItemDto;
-import com.sparta.springcore.service.ItemSearchService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
 
-@Controller
-public class ItemSearchController {
-
-    // Service 선언
-    private final ItemSearchService itemSearchService;
-
-    @Autowired
-    public ItemSearchController(ItemSearchService itemSearchService) {
-        this.itemSearchService = itemSearchService;
-    }
-
-    @GetMapping("/api/search")
-    @ResponseBody
-    public List<ItemDto> getItems(@RequestParam String query) throws IOException {
-        List<ItemDto> itemDtoList = itemSearchService.getItems(query);
-        return itemDtoList;
-    }
-
-    /*// Controller 가 자동으로 해주는 일
-    // 1. API Request 의 파라미터 값에서 검색어 추출 -> query 변수
-    // 5. API Response 보내기
-    //  5.1) response 의 header 설정
-    //  5.2) response 의 body 설정
-    @GetMapping("/api/search")
-    @ResponseBody
+@Service
+public class ItemSearchService {
     public List<ItemDto> getItems(@RequestParam String query) throws IOException {
         // 2. 네이버 쇼핑 API 호출에 필요한 Header, Body 정리
         RestTemplate rest = new RestTemplate();
@@ -57,5 +39,5 @@ public class ItemSearchController {
         }).readValue(itemsNode);
 
         return itemDtoList;
-    }*/
+    }
 }
