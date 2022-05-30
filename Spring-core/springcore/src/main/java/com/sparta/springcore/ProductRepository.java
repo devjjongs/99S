3ebd80be0c753bfe8fee19fcb618023fbbe940cd
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-30 13:51:25 +0900

    1-11~13강 AllInOneController의 코드 분리
    Controller, Service, Repository로 코드 분리

--- Spring-core/springcore/src/main/java/com/sparta/springcore/ProductRepository.java
+++ Spring-core/springcore/src/main/java/com/sparta/springcore/ProductRepository.java
@@ -1,21 +1,11 @@
 package com.sparta.springcore;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
-@RestController // JSON으로 데이터를 주고받음을 선언합니다.
-public class AllInOneController {
-
-    // 신규 상품 등록
-    @PostMapping("/api/products")
-    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
-
+public class ProductRepository {
+    public void createProduct(Product product) throws SQLException {
         // DB 연결
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:springcoredb", "sa", "");
 
@@ -42,14 +32,10 @@ public class AllInOneController {
         // DB 연결 해제
         ps.close();
         connection.close();
-
-        // 응답 보내기
-        return product;
     }
 
-    // 설정 가격 변경
-    @PutMapping("/api/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+    public Product getProduct(Long id) throws SQLException {
+        // 받은 데이터를 저장할 Product 객체 생성
         Product product = new Product();
 
         // DB 연결
@@ -68,29 +54,40 @@ public class AllInOneController {
             product.setLprice(rs.getInt("lprice"));
             product.setMyprice(rs.getInt("myprice"));
             product.setTitle(rs.getString("title"));
-        } else {
-            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
+
         }
+
+        // DB 연결 해제
+        ps.close();
+        connection.close();
+
+        // 가져온 정보 리턴
+        return product;
+    }
+
+    public void updateMyprice(Long id, int myprice) throws SQLException {
+        // DB 연결
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:springcoredb", "sa", "");
+
+        // DB Query 작성
+        PreparedStatement ps = connection.prepareStatement("select * from product where id = ?");
+        ps.setLong(1, id);
 
         // DB Query 작성
         ps = connection.prepareStatement("update product set myprice = ? where id = ?");
-        ps.setInt(1, requestDto.getMyprice());
-        ps.setLong(2, product.getId());
+
+        // 클라이언트에서 넘어온 myprice, id 전달
+        ps.setInt(1, myprice);
+        ps.setLong(2, id);
 
         // DB Query 실행
         ps.executeUpdate();
 
         // DB 연결 해제
-        rs.close();
         ps.close();
         connection.close();
-
-        // 응답 보내기 (업데이트된 상품 id)
-        return product.getId();
     }
 
-    // 등록된 전체 상품 목록 조회
-    @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
         List<Product> products = new ArrayList<>();
 
@@ -117,7 +114,7 @@ public class AllInOneController {
         rs.close();
         connection.close();
 
-        // 응답 보내기
+        // DB에서 전달받은 정보 리턴
         return products;
     }
 }
---
+++ Spring-core/springcore/src/main/java/com/sparta/springcore/ProductService.java
@@ -0,0 +1,53 @@
+package com.sparta.springcore;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+    // 관심 상품 등록 메소드
+    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
+        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto);
+
+        // Repository를 사용하기 위해 선언
+        ProductRepository productRepository = new ProductRepository();
+
+        // Repository의 createProduct() 메소드 호출
+        productRepository.createProduct(product);
+
+        // Product를 리턴
+        return product;
+    }
+
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
+        // Repository를 사용하기 위해 선언
+        ProductRepository productRepository = new ProductRepository();
+
+        // id 값으로 저장된 상품 가져오기
+        Product product = productRepository.getProduct(id);
+
+        // 에러 캐치
+        if (product == null) {
+            // 해당 id가 존재하지 않을 경우
+            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
+        }
+
+        // 클라이언트에서 전달받은 requestDto.getMyprice()를 id로 전달
+        productRepository.updateMyprice(id, requestDto.getMyprice());
+
+        // 업데이트된 product 정보 리턴
+        return product;
+    }
+
+    public List<Product> getProducts() throws SQLException {
+        // Repository를 사용하기 위해 선언
+        ProductRepository productRepository = new ProductRepository();
+
+        // id 값으로 저장된 상품 가져오기
+        List<Product> products = productRepository.getProducts();
+
+        // 전달받은 상품목록 리턴
+        return products;
+    }
+}
