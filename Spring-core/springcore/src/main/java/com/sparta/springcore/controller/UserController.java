ee9812cb6f4dc895b1c34444f5873a169046a673
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-31 14:39:42 +0900

    2-6 회원 가입 UI 반영
    로그인 / 회원가입 UI 반영 및  UI 이슈 해결을 위한 스프링 시큐리티의 WebSecurityConfig 클래스 URL 허용 정책 변경

---
+++ Spring-core/springcore/src/main/java/com/sparta/springcore/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.sparta.springcore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    // 회원 로그인 페이지
+    @GetMapping("/user/login")
+    public String login() {
+        // 로그인 페이지 리턴
+        return "login";
+    }
+
+    // 회원 가입 페이지
+    @GetMapping("/user/signup")
+    public String signup() {
+        // 회원 가입 페이지 리턴
+        return "signup";
+    }
+}
--- Spring-core/springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ Spring-core/springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -13,13 +13,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
                 // 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
                     // 로그인 기능 허용
                     .formLogin()
+                    .loginPage("/user/login")
                     // 로그인 성공시 루트 경로로 이동
                     .defaultSuccessUrl("/")
+                    .failureUrl("/user/login?error")
                     .permitAll()
                 .and()
                     // 로그아웃 기능 허용
