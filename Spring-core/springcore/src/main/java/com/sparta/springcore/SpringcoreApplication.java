c890df09bf339d7492a19aa0ae7fd22e074af5be
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-06-06 20:33:00 +0900

    3-3 JUnit을 이용한 단위 테스트
    테스트 코드 추가

--- Spring-core/springcore/src/main/java/com/sparta/springcore/SpringcoreApplication.java
+++ Spring-core/springcore/src/main/java/com/sparta/springcore/SpringcoreApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
+//@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 @EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
 @SpringBootApplication
